d1a8d9dcf4fcda8d0bb5b8270f79fa36d744243b
wizehunt <kriszt4@gmail.com>
2019-04-26 16:40:58 +0200

    updated loop

--- Abreuvoir_Connecte.ino
+++ Abreuvoir_Connecte.ino
@@ -2,6 +2,9 @@
 #include "RFID.h"
 #include "flow.h"
 
+#define PUMP 4
+#define VALVE_OUT 5
+#define VALVE_IN 6
 
 void setup() {
   setupPresenceDetector();
@@ -10,18 +13,19 @@ void setup() {
   Serial.begin(9600);
 
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(4
-  , OUTPUT);
+  pinMode(4 , OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(LED_BUILTIN, presenceDetected());
   digitalWrite(4, presenceDetected());
+  
   String id = readRFID();
   if(id != ""){
     Serial.println(id);  
   }
+  
   Serial.println(getPulse(0) + "\t" + String(analogRead(A7)));
   delay(100);
 
